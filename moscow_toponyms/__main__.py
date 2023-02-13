d509d07173a5bcc0133b86a9a211290d9ae67e68
diana-esaian <90722094+diana-esaian@users.noreply.github.com>
2023-02-13 21:38:12 +0400

    Add main function

--- moscow_toponyms/__main__.py
+++ moscow_toponyms/__main__.py
@@ -1,5 +1,18 @@
 from toponyms import *
 
-black_list = black_lists_constructor()[0]
-names_black_list = black_lists_constructor()[1]
+def main(book):
+    spacy_extractor = spacy_extract(book)
+    spacy_dict = spacy_extractor[0]
+    spacy_names = spacy_extractor[1]
 
+    natasha_extractor = natasha_extract(book)
+    natasha_dict = natasha_extractor[0]
+    natasha_names = natasha_extractor[1]
+
+    black_list = merging_blacklists(spacy_names, natasha_names)
+    final_results = inner_merging_filtering(black_list, spacy_dict, natasha_dict)
+    return final_results
+
+if __name__ == "__main__":
+    book = input()
+    main(book)
