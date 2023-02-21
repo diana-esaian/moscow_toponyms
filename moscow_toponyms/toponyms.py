edfd4f74fdfe5cccfac7d2aa264596c7a28792a7
diana-esaian <90722094+diana-esaian@users.noreply.github.com>
2023-02-21 04:30:28 +0400

    Update toponyms.py

--- moscow_toponyms/toponyms.py
+++ moscow_toponyms/toponyms.py
@@ -1,12 +1,11 @@
-import os 
+import os
 import json
-import pandas as pd
 import pymorphy2
 import spacy
 from natasha import (Segmenter, MorphVocab, NewsEmbedding, NewsMorphTagger,
                      NewsSyntaxParser, NewsNERTagger, Doc)
 # for spacy
-nlp = spacy.load("ru_core_news_sm")
+nlp = spacy.load(os.path.join(os.path.dirname(__file__), "ru_core_news_sm"))
 nlp.max_length = 20000000
 # for pymorphy2
 morph = pymorphy2.MorphAnalyzer()
@@ -20,72 +19,86 @@ ner_tagger = NewsNERTagger(emb)
 
 BASE_DIR = os.path.dirname(__file__)
 
-def spacy_extract(text):
-    spacy_dict = {}
-    spacy_names = {}
+class ExtractMosToponyms():
+    def spacy_extract(text):
+        spacy_dict = {}
+        spacy_names = {}
 
-    doc_spacy = nlp(text)
+        doc_spacy = nlp(text)
 
-    for ent in doc_spacy.ents:
-        if ent.label_ == 'LOC':
-            twice_lem = morph.parse(i)[0]
-            spacy_dict[ent.start_char] = twice_lem.normal_form
-        elif ent.label_ == 'PER':
-            spacy_names[ent.start_char] = ent.lemma_
-    return spacy_dict, spacy_names
+        for ent in doc_spacy.ents:
+            if ent.label_ == 'LOC':
+                twice_lem = morph.parse(ent.lemma_)[0]
+                spacy_dict[ent.start_char] = twice_lem.normal_form
+            elif ent.label_ == 'PER':
+                spacy_names[ent.start_char] = ent.lemma_
+        return spacy_dict, spacy_names
 
-def natasha_extract(text):  
-    natasha_dict = {}
-    natasha_names = {}
-                      
-    doc_natasha = Doc(text)
-    doc_natasha.segment(segmenter)
-    doc_natasha.tag_morph(morph_tagger)
-    doc_natasha.parse_syntax(syntax_parser)
-    doc_natasha.tag_ner(ner_tagger)
+    def natasha_extract(text):  
+        natasha_dict = {}
+        natasha_names = {}
+                        
+        doc_natasha = Doc(text)
+        doc_natasha.segment(segmenter)
+        doc_natasha.tag_morph(morph_tagger)
+        doc_natasha.parse_syntax(syntax_parser)
+        doc_natasha.tag_ner(ner_tagger)
 
-    for span in doc_natasha.spans:
-        if span.type == 'LOC':
-            span.normalize(morph_vocab)
-            natasha_dict[span.start] = [span.normal, span.stop, span.text]  
-        elif span.type == 'PER':
-            span.normalize(morph_vocab)
-            natasha_names[span.start] = (span.normal)
-    return natasha_dict, natasha_names
+        for span in doc_natasha.spans:
+            if span.type == 'LOC':
+                span.normalize(morph_vocab)
+                natasha_dict[span.start] = [span.normal, span.stop, span.text]  
+            elif span.type == 'PER':
+                span.normalize(morph_vocab)
+                natasha_names[span.start] = (span.normal)
+        return natasha_dict, natasha_names
 
-def merging_blacklists(spacy_names, natasha_names):
-    extracted_names = []
-    for i in spacy_names.keys():
-        position = i
-        if position in natasha_names.keys():
-            if natasha_names[position] not in extracted_names:
-                extracted_names.append(natasha_names[position])
-    with open(os.path.join(BASE_DIR, 'black_list.json')) as f:
-        black_list = json.load(f)
-    full_black_list = black_list + extracted_names
-    return full_black_list
+    def merging_blacklists(spacy_names, natasha_names):
+        extracted_names = []
+        for i in spacy_names.keys():
+            position = i
+            if position in natasha_names.keys():
+                if natasha_names[position] not in extracted_names:
+                    extracted_names.append(natasha_names[position])
+        with open(os.path.join(BASE_DIR, 'black_list.json')) as f:
+            black_list = json.load(f)
+        full_black_list = black_list + extracted_names
+        return full_black_list
 
-def inner_merging_filtering(full_black_list, spacy_dict, natasha_dict):
-    pre_final_spacy = {}
-    pre_final_natasha = {}
-    for i in spacy_dict.keys():
-        position = i
-        if position in natasha_dict.keys():
-            loc_n = natasha_dict[position][0]
-            loc_s = spacy_dict[position] # (the spelling can differ from loc_n after lemmatization)
-            if loc_n not in full_black_list:
-                pre_final_natasha[position] = [loc_n, natasha_dict[position][1], natasha_dict[position][2]]  
-            if loc_s not in full_black_list:
-                pre_final_spacy[position] = loc_s
-    
-    final_result = []
-    for i in pre_final_spacy.keys():
-        position = i
-        if position in pre_final_natasha.keys():
-            location_org = pre_final_natasha[position][2]
-            location_lem = pre_final_natasha[position][0]
-            start_value = position
-            stop_value = pre_final_natasha[position][1]
-            final_tuple = (location_org, location_lem, start_value, stop_value)
-            final_result.append(final_tuple)
-    return final_result
+    def inner_merging_filtering(full_black_list, spacy_dict, natasha_dict):
+        pre_final_spacy = {}
+        pre_final_natasha = {}
+        for i in spacy_dict.keys():
+            position = i
+            if position in natasha_dict.keys():
+                loc_n = natasha_dict[position][0]
+                loc_s = spacy_dict[position] # (the spelling can differ from loc_n after lemmatization)
+                if loc_n not in full_black_list:
+                    pre_final_natasha[position] = [loc_n, natasha_dict[position][1], natasha_dict[position][2]]  
+                if loc_s not in full_black_list:
+                    pre_final_spacy[position] = loc_s
+        
+        final_result = []
+        for i in pre_final_spacy.keys():
+            position = i
+            if position in pre_final_natasha.keys():
+                location_org = pre_final_natasha[position][2]
+                location_lem = pre_final_natasha[position][0]
+                start_value = position
+                stop_value = pre_final_natasha[position][1]
+                final_dict = {'toponym': location_org, 'lemmatized_toponym':location_lem, 'start_char':start_value, 'stop_char':stop_value}
+                final_result.append(final_dict)
+        return final_result 
+
+    def extract(book):
+        spacy_extractor = spacy_extract(book)
+        spacy_dict = spacy_extractor[0]
+        spacy_names = spacy_extractor[1]
+
+        natasha_extractor = natasha_extract(book)
+        natasha_dict = natasha_extractor[0]
+        natasha_names = natasha_extractor[1]
+
+        black_list = merging_blacklists(spacy_names, natasha_names)
+        final_results = inner_merging_filtering(black_list, spacy_dict, natasha_dict)
+        return final_results
